c879ecbe026864d8978b4610392a0ec9141796bc
nikelborm <evadev@duck.com>
2024-12-31 09:51:09 +0300

    quick save: Tue Dec 31 09:51:09 MSK 2024

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@
 
 import '@total-typescript/ts-reset';
 import { readFile, writeFile } from 'node:fs/promises';
+import { outdent } from 'outdent';
 import {
+  extractReposFromMarkdown,
   getEnvVarOrFail,
   IRepo,
   renderReposTableToMarkdown,
-  selfStarredReposOfUser
+  selfStarredReposOfUser,
+  splitStringApart
 } from './src/index.js';
 
 // const THEME = 'vision-friendly-dark'
@@ -16,46 +19,66 @@ const README_FILE_PATH = 'README.md'
 // this is also a default environment variable provided by Github Action
 const REPO_OWNER = getEnvVarOrFail('GITHUB_REPOSITORY_OWNER');
 const AMOUNT_OF_COLUMNS = 2;
+// I'm okay with loosing less than 20 percent of badges
+const FATAL_PERCENT_OF_REPOS_LOST_DUE_TO_API_ERRORS = 80;
 
-
-const reposCreatedByMe: IRepo[] = [];
-
-
-
-for await (const {
-  name,
-  owner: { login: owner }
-} of selfStarredReposOfUser(REPO_OWNER)) {
-  console.log(`Found own starred repo: ${name}`);
-
-  reposCreatedByMe.push({ name, owner });
-}
-
-const oldReadme = await readFile(README_FILE_PATH, 'utf8')
+const oldReadme = await readFile(README_FILE_PATH, 'utf8');
 
 console.log(`Finished reading source ${README_FILE_PATH} file`);
 
-const editableZoneStartsAt = oldReadme.indexOf(START_TOKEN);
-const editableZoneEndsAt = oldReadme.indexOf(END_TOKEN);
+const {
+  leftPartWithStartToken: nonEditableTopPart,
+  middlePartWithoutTokens: oldEditablePart,
+  rightPartWithEndToken: nonEditableBottomPart,
+} = splitStringApart({
+  initialStringToSplit: oldReadme,
+  startToken: START_TOKEN,
+  endToken: END_TOKEN,
+});
 
-if (editableZoneStartsAt === -1)
-  throw new Error('START marker is missing');
+let delayedError: Error | null = null;
+const reposCreatedByMe: IRepo[] = [];
 
-if (editableZoneEndsAt === -1)
-  throw new Error('END marker is missing');
+try {
+  for await (const {
+    name,
+    owner: { login: owner }
+  } of selfStarredReposOfUser(REPO_OWNER)) {
+    console.log(`Found own starred repo: ${name}`);
 
-if (editableZoneStartsAt > editableZoneEndsAt)
-  throw new Error(
-    'START marker cannot be set after END marker. START marker should go first.'
+    reposCreatedByMe.push({ name, owner });
+  }
+} catch (error) {
+  const passesGracefulDegradationCondition = reposCreatedByMe.length > (
+    extractReposFromMarkdown(oldEditablePart).length
+      * FATAL_PERCENT_OF_REPOS_LOST_DUE_TO_API_ERRORS / 100
   );
 
-const nonEditableTopPart = oldReadme.slice(0, editableZoneStartsAt + START_TOKEN.length);
-const nonEditableBottomPart = oldReadme.slice(editableZoneEndsAt);
+  if (error instanceof Error && passesGracefulDegradationCondition) {
+    console.error(outdent`
+      An error was thrown during fetching data from Github API, but already
+      fetched results will still be written to ${README_FILE_PATH} since condition
+      for graceful degradation was met. It means that before failing, API
+      returned more than ${FATAL_PERCENT_OF_REPOS_LOST_DUE_TO_API_ERRORS}% of the repos relative to previous CI run.
+    `);
+    delayedError = error;
+  } else throw new Error(
+    outdent`
+      There was an error during fetching data from Github API and condition
+      for graceful degradation wasn't met. It means that before failing API
+      returned LESS than ${FATAL_PERCENT_OF_REPOS_LOST_DUE_TO_API_ERRORS}% of the repos relative to previous CI run.
+    `,
+    { cause: error }
+  );
+}
+
 
 const newReadme = nonEditableTopPart
   + renderReposTableToMarkdown(reposCreatedByMe, AMOUNT_OF_COLUMNS)
-  + nonEditableBottomPart
+  + nonEditableBottomPart;
 
-await writeFile(README_FILE_PATH, newReadme)
+await writeFile(README_FILE_PATH, newReadme);
 
 console.log(`Finished writing result to ${README_FILE_PATH} file`);
+
+if (delayedError) throw delayedError;
