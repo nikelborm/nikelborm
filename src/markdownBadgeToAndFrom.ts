131e69e437dd89c5fe83b82234bc80d8d18ece37
nikelborm <evadev@duck.com>
2024-12-31 13:06:13 +0300

    quick save: Tue Dec 31 13:06:13 MSK 2024

--- src/markdownBadgeToAndFrom.ts
+++ src/markdownBadgeToAndFrom.ts
@@ -1,5 +1,7 @@
 import { IRepo } from './repo.interface.js';
 import { z } from 'zod';
+import { request } from 'undici';
+import { writeFile } from 'node:fs/promises';
 
 export function renderMarkdownRepoBadges(allRepos: IRepo[]) {
   return Promise.all(allRepos.map(
@@ -23,36 +25,36 @@ export async function renderRepoToMarkdownBadge(
     username: owner,
     repo: name,
     // tests
-    // title_color: '008088',
-    // text_color: '880800',
-    // icon_color: '444000',
-    // border_color: '202644',
-    // bg_color: '202020',
-    // serious
-    title_color: 'af7aff',
-    text_color: 'e4e4e4',
-    icon_color: 'af7aff',
-    bg_color: '010101',
-  })
+    title_color: '008088',
+    text_color: '880800',
+    icon_color: '444000',
+    border_color: '202644',
+    bg_color: '202020',
+    // nice theme
+    // title_color: 'af7aff',
+    // text_color: 'e4e4e4',
+    // icon_color: 'af7aff',
+    // bg_color: '010101',
+  });
 
-  // border color
-  // var(--borderColor-default,var(--color-border-default,#30363d))
+  const { statusCode, body } = await request(repoPreviewSvgImageURL)
 
-  // title color
-  // var(--fgColor-default, var(--color-fg-default))
+  if (statusCode !== 200)
+    throw new Error(`failed to fetch repo image for ${repoPreviewSvgImageURL}`);
 
-  // text color
-  // var(--fgColor-default, var(--color-fg-default))
+  const text = (await body.text())
+    .replaceAll('008088', 'var(--fgColor-default, var(--color-fg-default))')
+    .replaceAll('880800', 'var(--fgColor-default, var(--color-fg-default))')
+    .replaceAll('444000', 'var(--button-star-iconColor)')
+    .replaceAll('202644', 'var(--borderColor-default,var(--color-border-default,#30363d))')
+    .replaceAll('202020', 'var(--bgColor-default, var(--color-canvas-default))')
 
-  // icon color
-  // var(--button-star-iconColor)
-
-  // background color
-  // var(--bgColor-default, var(--color-canvas-default))
+  console.log(repoPreviewSvgImageURL, text);
+  await writeFile(`./images/${owner}_${name}.svg`, text);
 
   const repoURL = `https://github.com/${owner}/${name}`;
 
-  return `[![${name} repo](${repoPreviewSvgImageURL})](${repoURL})`;
+  return `[![${name} repo](./images/${owner}_${name}.svg)](${repoURL})`;
 }
 
 
