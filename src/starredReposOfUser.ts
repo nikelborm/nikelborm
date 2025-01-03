165e133743067f4ab8c7ac75f808b85793efa64c
nikelborm <evadev@duck.com>
2025-01-03 10:40:16 +0300

    quick save: Fri Jan  3 10:40:16 MSK 2025

--- src/starredReposOfUser.ts
+++ src/starredReposOfUser.ts
@@ -1,55 +1,75 @@
 import { Octokit } from '@octokit/core';
-import { ILinkHeader, parseLinkHeader } from './parseLinkHeader.js';
+import { parseLinkHeader } from './parseLinkHeader.js';
+import { racinglyIterateAll } from './racinglyIterateAll.js';
 import { IRepo } from './repo.interface.js';
 
-// Self imposed restriction to exit infinite loops
-const SENT_TOO_MUCH_REQUESTS_AMOUNT = 50;
-
 export async function* starredReposOfUser(username: string, per_page: number) {
+  console.log(`Started fetching pages of ${username}'s starred repos`);
+
+  const { linkHeader, repos } = await requestPageOfStarredRepos({
+    page: 1,
+    per_page,
+    username,
+  });
+
+  yield* repos;
+
+  const lastPage = linkHeader.last?.page || linkHeader.next?.page
+
+  console.log( `Fetched first out of ${lastPage ?? 1} pages`);
+
+  if (!lastPage) return;
+
+  for await (const { index, result: { repos } } of racinglyIterateAll(Array.from(
+    { length: lastPage - 1 },
+    (_, index) => requestPageOfStarredRepos({
+      page: index + 2,
+      per_page,
+      username,
+    })
+  ))) {
+    const page = index + 2;
+    console.log( `Racingly fetched ${ordinalSuffixOf(page)} page out of all ${lastPage} pages`);
+
+    yield* repos;
+  }
+}
+
+async function requestPageOfStarredRepos({
+  page,
+  username,
+  per_page,
+}: {
+  page: number,
+  username: string,
+  per_page: number,
+}) {
   if (per_page <= 0 || per_page > 100) throw new Error(
-    'Function statsOfUsers accepts only 0 < per_page <= 100'
+    'requestPageOfStarredRepos accepts only 0 < per_page <= 100'
   );
 
   if (!username) throw new Error(
-    'Function statsOfUsers accepts only non-empty strings as username'
+    'requestPageOfStarredRepos accepts only non-empty strings as username'
   );
 
   const octokit = new Octokit();
 
-  let sentAmountOfRequests = 0;
-  let doAnotherStep = true;
+  const { data, headers } = await octokit.request('GET /users/{username}/starred', {
+    username,
+    per_page,
+    direction: 'desc', // newest updated go first
+    page,
+    sort: 'updated',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    },
+  });
 
-  console.log(`Started fetching pages of ${username}'s starred repos`);
-
-  while (doAnotherStep && sentAmountOfRequests < SENT_TOO_MUCH_REQUESTS_AMOUNT) {
-    sentAmountOfRequests += 1;
-
-    const response = await octokit.request('GET /users/{username}/starred', {
-      username,
-      per_page,
-      direction: 'desc', // newest updated go first
-      page: sentAmountOfRequests,
-      sort: 'updated',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      },
-    });
-
-    let linkHeader: ILinkHeader;
-
-    try {
-      linkHeader = parseLinkHeader(response.headers.link);
-
-      const lastPage = linkHeader.last?.page;
-
-      console.log( `Fetched ${sentAmountOfRequests} out of ${lastPage ?? sentAmountOfRequests} pages`);
-    } catch (error) {
-      throw new Error(`Failed to parse link header fetched from ${sentAmountOfRequests} page`, { cause: error });
-    }
-
-    for (const elem of response.data) {
-      const repo = 'repo' in elem ? elem.repo : elem;
-      yield {
+  return {
+    linkHeader: parseLinkHeader(headers.link),
+    repos: data.map(e => {
+      const repo = 'repo' in e ? e.repo : e;
+      return {
         name: repo.name,
         owner: repo.owner.login,
         isItArchived: repo.archived,
@@ -59,9 +79,22 @@ export async function* starredReposOfUser(username: string, per_page: number) {
         lastTimeBeenPushedInto: repo.pushed_at
           ? new Date(repo.pushed_at)
           : null,
-      } satisfies IRepo;
-    }
-
-    doAnotherStep = !!linkHeader.next?.url;
+      } satisfies IRepo
+    })
   }
+}
+
+function ordinalSuffixOf(i: number) {
+  let j = i % 10,
+      k = i % 100;
+  if (j === 1 && k !== 11) {
+      return i + "st";
+  }
+  if (j === 2 && k !== 12) {
+      return i + "nd";
+  }
+  if (j === 3 && k !== 13) {
+      return i + "rd";
+  }
+  return i + "th";
 }
